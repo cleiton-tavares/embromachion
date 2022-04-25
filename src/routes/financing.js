bec21160e9c9f9ee8040d4cd1875de125976b49e
Fernando Santos <fernando.santos@ciandt.com>
2022-04-25 12:25:00 -0300

    Include warranties routes and model

--- src/routes/financing.js
+++ src/routes/financing.js
@@ -9,9 +9,11 @@ module.exports = ({express, controllers}) => {
     // REGULAR ROUTES
     express.get(`${baseApi}/contracts`, (req, res) => IndexController.sigleApi(model, 'contracts', req, res));
     express.get(`${baseApi}/contracts/:id`, (req, res) => IndexController.sigleApi(model, 'contractsId', req, res));
+    express.get(`${baseApi}/contracts/:id/warranties`, (req, res) => IndexController.sigleApi(model, 'warranties', req, res));
 
     // BATCH ROUTES
     express.get(`${baseApi}/contracts/batch`, (req, res) => IndexController.batchApi(model, 'contracts', req, res));
     express.get(`${baseApi}/contracts/:id/batch`, (req, res) => IndexController.batchApi(model, 'contractsId', req, res));
+    express.get(`${baseApi}/contracts/:id/warranties/batch`, (req, res) => IndexController.batchApi(model, 'warranties', req, res));
 
 };
