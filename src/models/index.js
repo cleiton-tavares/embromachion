9cdce73828fb76b499ad58319468e01bce76724a
Marcelo <marcelobc@ciandt.com>
2022-04-27 09:58:27 -0300

    Correcao arquivo de rotas

--- src/models/index.js
+++ src/models/index.js
@@ -3,11 +3,13 @@ const { CreditCard } = require('./creditCard');
 const { InvoiceFinancings } = require('./invoiceFinancings');
 const { Overdraft } = require('./opcredoverdraft');
 const { Customer } = require('./customer');
+const { Loans } = require('./opcredloans');
 
 module.exports = {
   Account,
   CreditCard,
   InvoiceFinancings,
   Overdraft,
-  Customer
+  Customer,
+  Loans
 };
