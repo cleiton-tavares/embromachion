197f684fe87ffa7d3b2a820f23f08a30dd41bed3
Marcelo <marcelobc@ciandt.com>
2022-04-27 09:50:29 -0300

    API nova e correcao da antiga

--- src/models/index.js
+++ src/models/index.js
@@ -1,13 +1,13 @@
 const { Account } = require('./account');
 const { CreditCard } = require('./creditCard');
 const { InvoiceFinancings } = require('./invoiceFinancings');
-const { Contracts } = require('./opcredoverdraft');
+const { Overdraft } = require('./opcredoverdraft');
 const { Customer } = require('./customer');
 
 module.exports = {
   Account,
   CreditCard,
   InvoiceFinancings,
-  Contracts,
+  Overdraft,
   Customer
 };
